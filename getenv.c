a359c50212f1680f59bbcce5d2b25d8ca9cf3fc2
nkanga1 <edonkanga@gmail.com>
2023-09-22 11:06:20 +0100

    Array of environ

--- getenv.c
+++ getenv.c
@@ -3,7 +3,7 @@
 /**
  * get_environ - returns the string array copy of our environ
  * @info: Structure containing potential arguments. Used to maintain
- * constant function prototype
+ *          constant function prototype.
  * Return: Always 0
  */
 char **get_environ(info_t *info)
@@ -19,9 +19,9 @@ char **get_environ(info_t *info)
 
 /**
  * _unsetenv - Remove an environment variable
- * @info: Structure containing npotential arguments. used to maintain
- * constant function prototype
- * Return: 1 on delete, 0 otherwise
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: 1 on delete, 0 otherwise
  * @var: the string env var property
  */
 int _unsetenv(info_t *info, char *var)
@@ -50,13 +50,13 @@ int _unsetenv(info_t *info, char *var)
 }
 
 /**
- * _setenv - Initialize a new environment variable
- * or modify an existing one
- * @info: Structure containing potential arguments. used to maintain
- * constant function prototype
+ * _setenv - Initialize a new environment variable,
+ *             or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
  * @var: the string env var property
  * @value: the string env var value
- * Return: Always 0
+ *  Return: Always 0
  */
 int _setenv(info_t *info, char *var, char *value)
 {
