e056c0158c91c539d55b466be7afb691cff244c3
nkanga1 <edonkanga@gmail.com>
2023-09-22 10:44:34 +0100

    Create shell_loop.c

--- shell_loop.c
+++ shell_loop.c
@@ -2,10 +2,10 @@
 
 /**
  * hsh - main shell loop
- * @info: the parametr & return info struct
+ * @info: the parameter & return info struct
  * @av: the argument vector from main()
  *
- * Return: 0 on success, 1 on error or error code
+ * Return: 0 on success, 1 on error, or error code
  */
 int hsh(info_t *info, char **av)
 {
@@ -36,7 +36,7 @@ int hsh(info_t *info, char **av)
 		exit(info->status);
 	if (builtin_ret == -2)
 	{
-		if (info->errr_num == -1)
+		if (info->err_num == -1)
 			exit(info->status);
 		exit(info->err_num);
 	}
@@ -44,13 +44,13 @@ int hsh(info_t *info, char **av)
 }
 
 /**
- * find_builtin - find a builtin command
- * @info: the paramet & return info struct
+ * find_builtin - finds a builtin command
+ * @info: the parameter & return info struct
  *
  * Return: -1 if builtin not found,
- * 0 if builtin executed successfully,
- * 1 if builtin found but not successful,
- * -2 if builtin signals exit()
+ *			0 if builtin executed successfully,
+ *			1 if builtin found but not successful,
+ *			-2 if builtin signals exit()
  */
 int find_builtin(info_t *info)
 {
@@ -77,7 +77,6 @@ int find_builtin(info_t *info)
 	return (built_in_ret);
 }
 
-
 /**
  * find_cmd - finds a command in PATH
  * @info: the parameter & return info struct
@@ -100,6 +99,7 @@ void find_cmd(info_t *info)
 			k++;
 	if (!k)
 		return;
+
 	path = find_path(info, _getenv(info, "PATH="), info->argv[0]);
 	if (path)
 	{
@@ -119,9 +119,8 @@ void find_cmd(info_t *info)
 	}
 }
 
-
 /**
- * fork_cmd - forks an exec thread to run cmd
+ * fork_cmd - forks a an exec thread to run cmd
  * @info: the parameter & return info struct
  *
  * Return: void
